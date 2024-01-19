270ac5e4029281fd78bf3a492adcdfd1045809a6
Anaellen Silva <anaellen.silva123@gmail.com>
2024-01-19 11:29:23 -0300

    Terceiro commit

--- view.py
+++ view.py
@@ -7,3 +7,19 @@ def inserir_info(i):
         cur = con.cursor()
         query = ("INSERT INTO formulario(nome, email, telefon, dia_, etado, assunto) VALUES(?, ?, ?, ?, ?, ?)")
         cur.execute(query,i)
+
+
+
+
+def mostrar_info():
+    with con:
+        lista = []
+        cur = con.cursor()
+        query = "SELECT * FROM formulario"
+        cur.execute(query)
+        informacao = cur.fetchall()
+
+        for i in informacao:
+            lista.append(i)
+
+    return lista
