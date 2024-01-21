4e1378603391984ff903068129783849294d3e83
Anaellen Silva <anaellen.silva123@gmail.com>
2024-01-21 20:36:51 -0300

    Quinto commit

---
+++ main.py
@@ -0,0 +1,47 @@
+#importando tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+
+
+# palheta de cores
+co0 = "#f0f3f5"  # Preta
+co1 = "#feffff"  # branca
+co2 = "#4fa882"  # verde
+co3 = "#38576b"  # valor
+co4 = "#403d3d"   # letra
+co5 = "#e06636"   # - profit
+co6 = "#038cfc"   # azul
+co7 = "#ef5350"   # vermelha
+co8 = "#263238"   # + verde
+co9 = "#e9edf5"   # sky blue
+
+
+##########criando a janela######
+
+janela = Tk()
+janela.title("")
+janela.geometry('1043x453')
+janela.configure(background=co9)
+janela.resizable(width=FALSE, height=FALSE)
+
+frame_cima = Frame(janela, width=310, height=50, bg=co2, relief='flat')
+frame_cima.grid(row=0, column=0)
+
+frame_baixo = Frame(janela, width=310, height=403, bg=co1, relief='flat')
+frame_baixo.grid(row=1, column=0, sticky=NSEW, padx=0, pady=1)
+
+
+frame_direita = Frame(janela, width=588, height=403, bg=co1, relief='flat')
+frame_direita.grid(row=0, column=1, rowspan=2, padx=1, sticky=NSEW)
+
+###########labe cima ##########
+app_nome = Label(frame_cima, text= 'formulário de consultoria' , anchor=NW, font=('Ivy 13 bold'), bg=co2, fg=co1 , relief='flat')
+app_nome.place(x=10, y=20)
+
+#varialvel tree global
+
+global tree
+
+janela.mainloop()
