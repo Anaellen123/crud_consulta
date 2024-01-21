f762a5dc39ec7bee67a4a8671630c1e13aac7573
Anaellen Silva <anaellen.silva123@gmail.com>
2024-01-21 17:54:14 -0300

    Quarto commit

--- view.py
+++ view.py
@@ -2,15 +2,16 @@ import sqlite3 as lite
 
 con = lite.connect('ficha.db')
 
+#inserir a informacao
 def inserir_info(i):
     with con:
         cur = con.cursor()
-        query = ("INSERT INTO formulario(nome, email, telefon, dia_, etado, assunto) VALUES(?, ?, ?, ?, ?, ?)")
-        cur.execute(query,i)
+        query = ("INSERT INTO formulario(nome, email, telefone, dia_em, estado, assunto) VALUES(?, ?, ?, ?, ?, ?)")
+        cur.execute(query, i)
 
 
 
-
+#acessar informações
 def mostrar_info():
     with con:
         lista = []
@@ -23,3 +24,17 @@ def mostrar_info():
             lista.append(i)
 
     return lista
+
+#atualizar informações
+def atualizar_info(i):
+    with con:
+        cur = con.cursor()
+        query = "UPDATE formulario SET nome=?, email=?, telefone=?, dia_em=?, estado=?, assunto=? WHERE id=?"
+        cur.execute(query, i)
+
+
+def deletar_info(i):
+    with con:
+        cur = con.cursor()
+        query = "DELETE FROM fromulario WHERE id=?"
+        cur.execute(query,i)
